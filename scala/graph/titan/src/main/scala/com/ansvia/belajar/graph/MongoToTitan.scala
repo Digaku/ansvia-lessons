6f6aa9273544749117dce8a66162145cabc2d04b
robin <robin@digaku.com>
2013-01-05 13:57:31 +0700

    updated

--- scala/graph/titan/src/main/scala/com/ansvia/belajar/graph/MongoToTitan.scala
+++ scala/graph/titan/src/main/scala/com/ansvia/belajar/graph/MongoToTitan.scala
@@ -7,9 +7,14 @@ import org.apache.commons.configuration.BaseConfiguration
 import com.ansvia.perf.PerfTiming
 import java.lang.Iterable
 import java.util
+import com.mongodb.casbah.MongoConnection
 
 
 object MongoToTitan extends PerfTiming {
+
+    lazy val con = MongoConnection()
+    lazy val db = con("digaku")
+    lazy val userCol = db("user")
 
     def main(args:Array[String]){
 
@@ -22,65 +27,7 @@ object MongoToTitan extends PerfTiming {
         conf.setProperty("storage.hostname", "localhost")
         val g = TitanFactory.open(conf)
 
-        val robin = g.addVertex(null)
-        robin.setProperty("name", "robin")
-
-        val andrie = g.addVertex(null)
-        andrie.setProperty("name", "andrie")
-
-        val temon = g.addVertex(null)
-        temon.setProperty("name", "temon")
-
-        val adit = g.addVertex(null)
-        adit.setProperty("name", "Adit")
-
-
-        val supportE = g.addEdge(null, robin, andrie, "support")
-        val supportE2 = g.addEdge(null, robin, temon, "support")
-        val supportE3 = g.addEdge(null, adit, andrie, "support")
-
-        var result: Iterable[Vertex] = null
-        var it: util.Iterator[Vertex] = null
-
-        timing("Who is supported by robin"){
-            result = robin.query().labels("support").vertices()
-            it = result.iterator()
-            while(it.hasNext){
-                val v = it.next()
-                println(" + " + v.getProperty("name"))
-            }
-        }
-
-
-        timing("Who is supported by robin with cool"){
-            supportE.setProperty("cool", true)
-
-            result = robin.query().labels("support").has("cool", true).vertices()
-            it = result.iterator()
-            while (it.hasNext){
-                val v = it.next()
-                println(" + " + v.getProperty("name"))
-            }
-        }
-
-
-        timing("who is supporting andrie"){
-            it = andrie.getVertices(Direction.IN, "support").iterator()
-            while(it.hasNext){
-                val v = it.next()
-                println(" + " + v.getProperty("name"))
-            }
-        }
-
-        g.removeVertex(andrie)
-        g.removeVertex(temon)
-        g.removeVertex(adit)
-        g.removeVertex(robin)
-
-//        g.removeEdge(supportE)
-//        g.removeEdge(supportE2)
-//        g.removeEdge(supportE3)
-
+        g.createKeyIndex("name", classOf[Vertex])
 
     }
 }
